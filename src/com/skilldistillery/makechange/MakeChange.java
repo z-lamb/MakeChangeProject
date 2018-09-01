9de50b23f4adccc7c53def909bb5695c79ac324b
z-lamb <lamb.zachary@gmail.com>
2018-09-01 10:36:52 -0600

    working without mod

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -9,13 +9,15 @@ public class MakeChange {
 		//  ask how much user needs to pay
 		Scanner tender = new Scanner(System.in);
 		System.out.println("How much do you need to pay?");
-		double amountRequired = tender.nextDouble();
-		
+		double amountR = tender.nextDouble();
+		int amountRequired = (int) (amountR * 100);
+//		System.out.println(amountRequired);
 		
 		//  ask how much user is tendering
 		System.out.println("Please enter the amount being given");
-		double amountTendered = tender.nextDouble();
-		
+		double amountT = tender.nextDouble();
+		int amountTendered = (int) (amountT * 100);
+//		System.out.println(amountTendered);
 		
 		//  display message if too little is entered
 		if ( amountRequired == amountTendered ) {
@@ -35,59 +37,66 @@ public class MakeChange {
 		
 	}
 	
-	public static String changeMaker(double amountRequired, double amountTendered) {
+	public static String changeMaker(int amountRequired, int amountTendered) {
 		String changeMaker = "";
-		double amountToChange = amountTendered - amountRequired;
+		int amountToChange = amountTendered - amountRequired;
 		
-		int totalChange = (int) (amountToChange * 100); 
+		
 		int bills20, bills10, bills5, bills1, coins25, coins10, coins5, coins1;
 		System.out.println();
 		
-		if(totalChange >= 2000) {
-			bills20 = (int) (totalChange / 2000);
+//		System.out.println(1-.67);
+//		System.out.println(amountRequired);
+//		System.out.println(amountTendered);
+//		System.out.println(amountToChange);
+		if(amountToChange >= 2000) {
+			bills20 = (int) (amountToChange / 2000);
 			changeMaker = changeMaker + (bills20 + " Twenty Dollar bills\n");
-			totalChange = totalChange - (bills20 * 2000);
+			amountToChange = amountToChange - (bills20 * 2000);
+//			System.out.println(amountToChange);
 		}
-		if(totalChange >= 1000) {
-			bills10 = (int) (totalChange / 1000);
+		if(amountToChange >= 1000) {
+			bills10 = (int) (amountToChange / 1000);
 			changeMaker = changeMaker + (bills10 + " Ten Dollar bills\n");
-			totalChange = totalChange - (bills10 * 1000);
+			amountToChange = amountToChange - (bills10 * 1000);
+//			System.out.println(amountToChange);
 		}
-		if(totalChange >= 500) {
-			bills5 = (int) (totalChange / 500);
+		if(amountToChange >= 500) {
+			bills5 = (int) (amountToChange / 500);
 			changeMaker = changeMaker + (bills5 + " Five Dollar bills\n");
-			totalChange = totalChange - (bills5 * 500);
+			amountToChange = amountToChange - (bills5 * 500);
+//			System.out.println(amountToChange);
 		}
-		if(totalChange >= 100) {
-			bills1 = (int) (totalChange / 100);
+		if(amountToChange >= 100) {
+			bills1 = (int) (amountToChange / 100);
 			changeMaker = changeMaker + (bills1 + " One Dollar bills\n");
-			totalChange = totalChange - (bills1 * 100);
+			amountToChange = amountToChange - (bills1 * 100);
+//			System.out.println(amountToChange);
 		}
-		
-		System.out.println(totalChange);
-		if(totalChange >= 25) {
-			coins25 = (int) (totalChange / 25);
+		if(amountToChange >= 25) {
+			coins25 = (int) (amountToChange / 25);
 			changeMaker = changeMaker + (coins25 + " Quarters\n");
-			totalChange = totalChange - (coins25 * 25);
-			System.out.println(totalChange);
+			amountToChange = amountToChange - (coins25 * 25);
+//			System.out.println(amountToChange);
 		}
-		if(totalChange >= 10) {
-			coins10 = (int) (totalChange / 10);
+		if(amountToChange >= 10) {
+			coins10 = (int) (amountToChange / 10);
 			changeMaker = changeMaker + (coins10 + " Dimes\n");
-			totalChange = totalChange - (coins10 * 10);
+			amountToChange = amountToChange - (coins10 * 10);
+//			System.out.println(amountToChange);
 		}
-		if(totalChange >= 5) {
-			coins5 = (int) (totalChange / 5);
+		if(amountToChange >= 5) {
+			coins5 = (int) (amountToChange / 5);
 			changeMaker = changeMaker + (coins5 + " Nickels\n");
-			totalChange = totalChange - (coins5 * 5);
-			System.out.println(totalChange);
+			amountToChange = amountToChange - (coins5 * 5);
+//			System.out.println(amountToChange);
 			
 		}
-		if(totalChange >= 1) {
-			coins1 = (int) (totalChange / 1);
+		if(amountToChange >= 1) {
+			coins1 = (int) (amountToChange / 1);
 			changeMaker = changeMaker + (coins1 + " Pennies\n");
-			totalChange = totalChange - (coins1 * 1);
-			System.out.println(totalChange);
+			amountToChange = amountToChange - (coins1 * 1);
+//			System.out.println(amountToChange);
 		}
 
 		
