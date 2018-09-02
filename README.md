d6e132d9ccd18ebf1826fd8c4fec2596408b6a0e
z-lamb <lamb.zachary@gmail.com>
2018-09-02 17:26:03 -0600

    updated README / print output

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
 ## Make Change Project
 
 ### Week One homework for Skill Distillery
+
+#### Summary:
+This program takes the input from the user for how much their purchase price is and how much they are tendering to pay for the purchase. If the purchase is equal to the amount being tendered the program will display a message and close. If the amount being tendered is less than what is needed the user will have the chance to give more until enough has been given for the purchase price. If the user is not willing to be enter enough for the purchase a message will be given and the program will exit. If at any point the user gives more than the required amount the program will display the total amount of change being given and exit.
+
+#### Operation:
+Enter amounts in standard USD format.
+Enter response to questions in either "Y" for Yes or "N" for No.
+
+#### Processes:
+Not all of these were required or would make the program more efficient  but were added to practice using them
+Methods
+Array
+Scanner
+Printing
+While Loop
+If Statement
+Switch
+Double
+Int
+Mathematical Operators
+Relational Operators
+Concatenation
+
+#### Overview:
+- From the main method call requests method
+- From the requests method
+ - Create an array to hold the two values of amount required and amount being tendered
+ - Request amount required as a double, convert to int and store in array[0] as total amount in pennies
+ - Request amount being tendered as a double, convert to int and store in array[1] as total amount in pennies
+ - Loop if amount tendered is equal to or less than amount required
+  - If amount being tendered is equal to amount required print out message and close program
+  - Else amount being tendered is less than amount required and ask if they would like more
+   - Use switch to accommodate different ways Yes and No can be entered
+   - If Yes is entered ask for how much more will be given and add it to array[1]
+   - If No is entered print out message and close the program
+   - If anything else is entered it will print a message giving instructions
+ - If at any point the amount being tendered is more than the amount required call changeMaker method and send array
+  -Declare String that will return the string of change
+  -Declare ints needed to calculate
+  -Take amounts from the array and calculate amount of change needed
+  -Print amount tendered, amount required, and change as converted to doubles and printed to USD format
+  -Use if statements to determine what denomination is needed for change
+   -Use if/else statements if more than 1 of a denomination can be given back to have proper plurality
+  -Return string back to requests method
+ -Print final message
+-Close Program
+  
--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -1,107 +1,161 @@
 package com.skilldistillery.makechange;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class MakeChange {
 
 	public static void main(String[] args) {
+		
+		// calls requests() method to start the program
+		requests();
 
-		//  ask how much user needs to pay
-		Scanner tender = new Scanner(System.in);
-		System.out.println("How much do you need to pay?");
-		double amountR = tender.nextDouble();
-		int amountRequired = (int) (amountR * 100);
-//		System.out.println(amountRequired);
-		
-		//  ask how much user is tendering
-		System.out.println("Please enter the amount being given");
-		double amountT = tender.nextDouble();
-		int amountTendered = (int) (amountT * 100);
-//		System.out.println(amountTendered);
-		
-		//  display message if too little is entered
-		if ( amountRequired == amountTendered ) {
-			System.out.println("Thank you for giving exact change");
-		}
-		else if (amountRequired >= amountTendered) {
-			System.out.println("You did not enter the correct amount. Please go away!");
-		}
-		else {
-			String changeMaker = changeMaker(amountRequired, amountTendered);
-			System.out.println(changeMaker);
-		}
-		
-		//  display message if exact amount is tendered
-		
-		//  return how much change is being given
-		
 	}
-	
-	public static String changeMaker(int amountRequired, int amountTendered) {
-		String changeMaker = "";
-		int amountToChange = amountTendered - amountRequired;
-		
-		
-		int bills20, bills10, bills5, bills1, coins25, coins10, coins5, coins1;
-		System.out.println();
-		
-//		System.out.println(1-.67);
-//		System.out.println(amountRequired);
-//		System.out.println(amountTendered);
-//		System.out.println(amountToChange);
-		if(amountToChange >= 2000) {
-			bills20 = (int) (amountToChange / 2000);
-			changeMaker = changeMaker + (bills20 + " Twenty Dollar bills\n");
-			amountToChange = amountToChange - (bills20 * 2000);
-//			System.out.println(amountToChange);
-		}
-		if(amountToChange >= 1000) {
-			bills10 = (int) (amountToChange / 1000);
-			changeMaker = changeMaker + (bills10 + " Ten Dollar bills\n");
-			amountToChange = amountToChange - (bills10 * 1000);
-//			System.out.println(amountToChange);
-		}
-		if(amountToChange >= 500) {
-			bills5 = (int) (amountToChange / 500);
-			changeMaker = changeMaker + (bills5 + " Five Dollar bills\n");
-			amountToChange = amountToChange - (bills5 * 500);
-//			System.out.println(amountToChange);
-		}
-		if(amountToChange >= 100) {
-			bills1 = (int) (amountToChange / 100);
-			changeMaker = changeMaker + (bills1 + " One Dollar bills\n");
-			amountToChange = amountToChange - (bills1 * 100);
-//			System.out.println(amountToChange);
-		}
-		if(amountToChange >= 25) {
-			coins25 = (int) (amountToChange / 25);
-			changeMaker = changeMaker + (coins25 + " Quarters\n");
-			amountToChange = amountToChange - (coins25 * 25);
-//			System.out.println(amountToChange);
-		}
-		if(amountToChange >= 10) {
-			coins10 = (int) (amountToChange / 10);
-			changeMaker = changeMaker + (coins10 + " Dimes\n");
-			amountToChange = amountToChange - (coins10 * 10);
-//			System.out.println(amountToChange);
-		}
-		if(amountToChange >= 5) {
-			coins5 = (int) (amountToChange / 5);
-			changeMaker = changeMaker + (coins5 + " Nickels\n");
-			amountToChange = amountToChange - (coins5 * 5);
-//			System.out.println(amountToChange);
-			
-		}
-		if(amountToChange >= 1) {
-			coins1 = (int) (amountToChange / 1);
-			changeMaker = changeMaker + (coins1 + " Pennies\n");
-			amountToChange = amountToChange - (coins1 * 1);
-//			System.out.println(amountToChange);
-		}
+
+	private static void requests() {
+
+		// requests amounts for purchase price and amount being tendered and stores it in an array
+		int[] requestsArr = new int[2];
+
+		Scanner tender = new Scanner(System.in);
+		System.out.print("Please input the total for your purchase:  $");
+		double amountR = tender.nextDouble();
+		requestsArr[0] = (int) (amountR * 100);
+
+		System.out.print("Please enter the amount being tendered:  $");
+		double amountT = tender.nextDouble();
+		requestsArr[1] = (int) (amountT * 100);
 
 		
-//		System.out.println(totalChange);
+//		checks to see if amount given is less than or equal to the purchase price
+//		if amount is equal to purchase price the program will display a message and quit
+//		if amount is is less than you will have the option to add more until enough or more has been given
+//		if user will not give enough the program will display a message and quit
+		while (requestsArr[0] >= requestsArr[1]) {
+			if (requestsArr[0] == requestsArr[1]) {
+				System.out.println("Thank you for giving exact change. Enjoy your purchase! ");
+				tender.close();
+				return;
+			}
+			else {
+				System.out.print("You did not enter the correct amount. Would you like to add more? [Y]es / [N]o >> ");
+				String extra = tender.next();
+				switch (extra) {
+				case "Y":
+				case "y":
+				case "Yes":
+				case "yes":
+					System.out.print("How much more will you be tendering: $");
+					amountT = tender.nextDouble();
+					requestsArr[1] = requestsArr[1] + (int) (amountT * 100);
+					break;
+
+				case "N":
+				case "n":
+				case "No":
+				case "no":
+					System.out.println("\nYou did not enter enough. We do not give our products to cherity.");
+					System.out.println("Please take your $" + ((double)requestsArr[1]/100) + " and go away!");
+					tender.close();
+					return;
+				default:
+					System.out.println("Please enter Y or N\n");
+				}
+			}
+		}
 		
+		
+//		calls changeMaker() method to process change and returns to print out amount of change being given
+		String changeMakerPrint = changeMaker(requestsArr);
+		System.out.println(changeMakerPrint);
+		System.out.println("Enjoy your purchase!");
+
+		tender.close();
+	}
+
+	public static String changeMaker(int requestsArr[]) {
+	
+//		processes the change to be given as a result of tendering more than the purchase price
+//		which adds to the string that is being returned to print the final change
+//		the proper plural response will given if there are more than 1 denomination being given
+		String changeMaker = "";
+		int bills20, bills10, bills5, bills1, coins25, coins10, coins5, coins1;
+		int amountToChange = requestsArr[1] - requestsArr[0];
+
+		DecimalFormat df = new DecimalFormat("#.00");
+		System.out.println();
+		System.out.println("Amount Tendered: $" + df.format((double)requestsArr[1] / 100));
+		System.out.println("Amount Required: $" + df.format((double)requestsArr[0] / 100));
+		System.out.println("Total Change: $" + df.format((double)amountToChange / 100));
+		System.out.println();
+		System.out.println("Your change is:");
+
+		if (amountToChange >= 2000) {
+			bills20 = amountToChange / 2000;
+			if (bills20 == 1) {
+				changeMaker = changeMaker + (bills20 + " Twenty Dollar Bill\n");
+			}
+			else {
+				changeMaker = changeMaker + (bills20 + " Twenty Dollar Bills\n");
+			}
+			amountToChange = amountToChange - (bills20 * 2000);
+		}
+		if (amountToChange >= 1000) {
+			bills10 = amountToChange / 1000;
+			changeMaker = changeMaker + (bills10 + " Ten Dollar Bill\n");
+			amountToChange = amountToChange - (bills10 * 1000);
+		}
+		if (amountToChange >= 500) {
+			bills5 = amountToChange / 500;
+			changeMaker = changeMaker + (bills5 + " Five Dollar Bill\n");
+			amountToChange = amountToChange - (bills5 * 500);
+		}
+		if (amountToChange >= 100) {
+			bills1 = amountToChange / 100;
+			if (bills1 == 1) {
+				changeMaker = changeMaker + (bills1 + " One Dollar Bill\n");
+			}
+			else {
+				changeMaker = changeMaker + (bills1 + " One Dollar Bills\n");
+			}
+			amountToChange = amountToChange - (bills1 * 100);
+		}
+		if (amountToChange >= 25) {
+			coins25 = amountToChange / 25;
+			if (coins25 == 1) {
+				changeMaker = changeMaker + (coins25 + " Quarter\n");
+			}
+			else {
+				changeMaker = changeMaker + (coins25 + " Quarters\n");
+			}
+			amountToChange = amountToChange - (coins25 * 25);
+		}
+		if (amountToChange >= 10) {
+			coins10 = amountToChange / 10;
+			if (coins10 == 1) {
+				changeMaker = changeMaker + (coins10 + " Dime\n");
+			}
+			else {
+				changeMaker = changeMaker + (coins10 + " Dimes\n");
+			}
+			amountToChange = amountToChange - (coins10 * 10);
+		}
+		if (amountToChange >= 5) {
+			coins5 = amountToChange / 5;
+			changeMaker = changeMaker + (coins5 + " Nickel\n");
+			amountToChange = amountToChange - (coins5 * 5);
+		}
+		if (amountToChange >= 1) {
+			coins1 = amountToChange / 1;
+			if (coins1 == 1) {
+				changeMaker = changeMaker + (coins1 + " Penny\n");
+			}
+			else {
+				changeMaker = changeMaker + (coins1 + " Pennies\n");
+			}
+			amountToChange = amountToChange - (coins1 * 1);
+		}
+
 		return changeMaker;
 	}
 
